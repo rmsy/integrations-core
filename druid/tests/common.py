be7a129c65bc0d76c6e9e24c8ea92eafa348f4c5
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-16 17:03:25 +0200

    Clean up common.py

--- druid/tests/common.py
+++ druid/tests/common.py
@@ -1,13 +1,6 @@
 # (C) Datadog, Inc. 2010-2018
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-from datadog_checks.dev import get_docker_hostname
-
-HOST = get_docker_hostname()
-PORT = '5432'
-USER = 'datadog'
-PASSWORD = 'datadog'
-DB_NAME = 'datadog_test'
 
 
 COORDINATOR_URL = 'http://localhost:8081'
